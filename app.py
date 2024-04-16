20dc87d9b511ce671f4430b97b6e3101029f26b6
Peyton Meyer <peytonmeyer23@gmail.com>
2024-04-16 08:29:00 -0400

    Changed  alot :p

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine, text
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Florg!'
+    return render_template('Index.html')
+
+@app.route('/base')
+def test():
+    return render_template('base.html')
+
+@app.route('/MyAccount')
+def Account():    
+     return render_template('MyAccount.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
