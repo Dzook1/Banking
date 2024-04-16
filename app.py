38dc5482de477cfd10e20d2c95b0fb7be0afbe8f
Peyton Meyer <peytonmeyer23@gmail.com>
2024-04-16 14:09:38 -0400

    changes

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 from sqlalchemy import create_engine, text
 app = Flask(__name__)
 
+conn_str = 'mysql://root:9866@localhost/banking'
+engine = create_engine(conn_str, echo=True)
+conn = engine.connect()
+
 @app.route('/')
 def home():
     return render_template('Index.html')
@@ -13,6 +17,24 @@ def test():
 @app.route('/MyAccount')
 def Account():    
      return render_template('MyAccount.html')
+
+@app.route('/MyAccount', methods=['GET'])
+def Account():
+    return render_template('MyAccount.html')
+
+
+@app.route('/MyAccount', methods=['POST'])
+def Accountin():
+    conn.execute(text('Select Balance from Information where user'), request.form)
+    conn.commit()
+    return render_template('MyAccount.html')
+
+
+
+@app.route('/Transfer')
+def transfer():    
+     return render_template('transfer.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
